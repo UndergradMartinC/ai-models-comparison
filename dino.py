0f2186cdedc4196e98a28e165e36a7ea68fa4e35
Martin Cook <martin.cook2019@gmail.com>
2025-09-15 13:18:04 -0500

    added time function to main

--- dino.py
+++ dino.py
@@ -1,1 +1,8 @@
-print("dino")
+import time
+
+def dino():
+    """Main DINO function"""
+    print("Running DINO model...")
+    # Simulate some processing time
+    time.sleep(0.1)
+    return "DINO completed"
